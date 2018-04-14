7d132d9e7afdfaef48a1b0ba46dcbdaef5bc2cac
Murphy <murphyjstevens@gmail.com>
2018-04-13 22:45:29 -0500

    Add login/signup system and socket io

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,25 +1,67 @@
-// Get the modal
+// Login Modal
 var modal = document.getElementById('login-modal-id');
-
-// Get the button that opens the modal
 var btn = document.getElementById("login-button-id");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
 btn.onclick = function() {
     modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
+
+//Socket IO
+var socket = io();
+var userToken;
+
+jQuery('#login-submit').on('click', function (e) {
+  e.preventDefault();
+  console.log('Login clicked');
+
+  var emailTextbox = jQuery('[name=email]');
+  var passwordTextbox = jQuery('[name=password]')
+
+  socket.emit('login', {
+    email: emailTextbox.val(),
+    password: passwordTextbox.val()
+  }, function (err, token) {
+    if (err) {
+      return console.log('Error', err);
+    }
+    console.log('Token', token);
+    userToken = token;
+    modal.style.display = "none";
+  });
+});
+
+jQuery('#signup-submit').on('click', function (e) {
+  e.preventDefault();
+  console.log('Signup clicked');
+
+  var emailTextbox = jQuery('[name=email]');
+  var passwordTextbox = jQuery('[name=password]')
+
+  socket.emit('signup', {
+    email: emailTextbox.val(),
+    password: passwordTextbox.val()
+  }, function (err, token) {
+    if (err) {
+      return console.log('Error', err);
+    }
+    console.log('Token', token);
+    userToken = token;
+    modal.style.display = "none";
+  });
+});
+
+socket.on('disconnect', function () {
+  console.log('Disconnected from server');
+  userToken = undefined;
+});
--- server/server.js
+++ server/server.js
@@ -1,151 +1,183 @@
 require('./config/config');
 
-const _ = require('lodash');
-const express = require('express');
 const bodyParser = require('body-parser');
+const express = require('express');
+const http = require('http');
+const _ = require('lodash');
 const {ObjectID} = require('mongodb');
 const path = require('path');
+const socketIO = require('socket.io');
 
+const {authenticate} = require('./middleware/authenticate');
 const {mongoose} = require('./db/mongoose')
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
-const {authenticate} = require('./middleware/authenticate');
 
-var app = express();
 const port = process.env.PORT;
 const publicPath = path.join(__dirname, '/../public');
+
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(bodyParser.json());
 app.use(express.static(publicPath));
 
-app.post('/todos', authenticate, (req, res) => {
-  var todo = new Todo({
-    text: req.body.text,
-    _creator: req.user._id
-  });
-
-  todo.save().then((doc) => {
-    res.send(doc);
-  }, (e) => {
-    res.status(400).send(e);
-  });
-});
-
-app.get('/todos', authenticate, (req, res) => {
-  Todo.find({
-    _creator: req.user._id
-  }).then((todos) => {
-    res.send({todos});
-  }, (e) => {
-    res.status(400).send(e);
-  });
-});
-
-app.get('/todos/:id', authenticate, (req, res) => {
-  var id = req.params.id;
-  if (!ObjectID.isValid(id)) {
-    return res.status(404).send();
-  }
-
-  Todo.findOne({
-    _id: id,
-    _creator: req.user._id
-  }).then((todo) => {
-    if (!todo) {
-      return res.status(404).send();
-    }
-    res.send({todo});
-  }, (e) => {
-    res.status(400).send();
-  });
-});
-
-app.delete('/todos/:id', authenticate, (req, res) => {
-  var id = req.params.id;
-  if (!ObjectID.isValid(id)) {
-    return res.status(404).send();
-  }
-
-  Todo.findOneAndRemove({
-    _id: id,
-    _creator: req.user._id
-  }).then((todo) => {
-    if (!todo) {
-      return res.status(404).send();
-    }
-    return res.send({todo});
-  }, (e) => {
-    res.status(400).send();
-  });
-});
-
-app.patch('/todos/:id', authenticate, (req, res) => {
-  var id = req.params.id;
-  var body = _.pick(req.body, ['text', 'completed']);
-
-  if (!ObjectID.isValid(id)) {
-    return res.status(404).send();
-  }
-
-  if (_.isBoolean(body.completed) && body.completed) {
-    body.completedAt = new Date().getTime();
-  } else {
-    body.completed = false;
-    body.completedAt = null;
-  }
-
-  Todo.findOneAndUpdate({
-    _id: id,
-    _creator: req.user._id
-  }, {$set: body}, {new: true}).then((todo) => {
-    if (!todo) {
-      return res.status(404).send();
-    }
-
-    res.send({todo});
-  }).catch((e) => {
-    res.status(400).send();
-  });
-});
-
-app.post('/users', (req, res) => {
-  var body = _.pick(req.body, ['email', 'password']);
-  var user = new User(body);
-
-  user.save().then(() => {
-    return user.generateAuthToken();
-  }).then((token) => {
-    res.header('x-auth', token).send(user);
-  }).catch((e) => {
-    res.status(400).send(e);
-  });
-});
-
-app.get('/users/me', authenticate, (req, res) => {
-  res.send(req.user);
-});
-
-app.post('/users/login', (req, res) => {
-  var body = _.pick(req.body, ['email', 'password']);
-
-  User.findByCredentials(body.email, body.password).then((user) => {
-    return user.generateAuthToken().then((token) => {
-      res.header('x-auth', token).send(user);
+io.on('connection', (socket) => {
+  socket.on('login', (params, callback) => {
+    User.findByCredentials(params.email, params.password).then((user) => {
+      return user.generateAuthToken();
+    }).then((token) => {
+      console.log('token', token);
+      callback(null, token);
+    }).catch((err) => {
+      callback(err);
     });
-  }).catch((e) => {
-    res.status(400).send();
+  });
+
+  socket.on('signup', (params, callback) => {
+    var user = new User({email: params.email, password: params.password});
+    console.log(`User created with email "${user.email}" and password "${user.password}"`);
+
+    user.save().then(() => {
+      return user.generateAuthToken();
+    }).then((token) => {
+      console.log('token', token);
+      callback(null, token);
+    }).catch((err) => {
+      callback(err);
+    });
   });
 });
 
-app.delete('/users/me/token', authenticate, (req, res) => {
-  req.user.removeToken(req.token).then(() => {
-    res.status(200).send();
-  }, (e) => {
-    res.status(400).send();
-  });
-});
+// app.post('/todos', authenticate, (req, res) => {
+//   var todo = new Todo({
+//     text: req.body.text,
+//     _creator: req.user._id
+//   });
+//
+//   todo.save().then((doc) => {
+//     res.send(doc);
+//   }, (e) => {
+//     res.status(400).send(e);
+//   });
+// });
+//
+// app.get('/todos', authenticate, (req, res) => {
+//   Todo.find({
+//     _creator: req.user._id
+//   }).then((todos) => {
+//     res.send({todos});
+//   }, (e) => {
+//     res.status(400).send(e);
+//   });
+// });
+//
+// app.get('/todos/:id', authenticate, (req, res) => {
+//   var id = req.params.id;
+//   if (!ObjectID.isValid(id)) {
+//     return res.status(404).send();
+//   }
+//
+//   Todo.findOne({
+//     _id: id,
+//     _creator: req.user._id
+//   }).then((todo) => {
+//     if (!todo) {
+//       return res.status(404).send();
+//     }
+//     res.send({todo});
+//   }, (e) => {
+//     res.status(400).send();
+//   });
+// });
+//
+// app.delete('/todos/:id', authenticate, (req, res) => {
+//   var id = req.params.id;
+//   if (!ObjectID.isValid(id)) {
+//     return res.status(404).send();
+//   }
+//
+//   Todo.findOneAndRemove({
+//     _id: id,
+//     _creator: req.user._id
+//   }).then((todo) => {
+//     if (!todo) {
+//       return res.status(404).send();
+//     }
+//     return res.send({todo});
+//   }, (e) => {
+//     res.status(400).send();
+//   });
+// });
+//
+// app.patch('/todos/:id', authenticate, (req, res) => {
+//   var id = req.params.id;
+//   var body = _.pick(req.body, ['text', 'completed']);
+//
+//   if (!ObjectID.isValid(id)) {
+//     return res.status(404).send();
+//   }
+//
+//   if (_.isBoolean(body.completed) && body.completed) {
+//     body.completedAt = new Date().getTime();
+//   } else {
+//     body.completed = false;
+//     body.completedAt = null;
+//   }
+//
+//   Todo.findOneAndUpdate({
+//     _id: id,
+//     _creator: req.user._id
+//   }, {$set: body}, {new: true}).then((todo) => {
+//     if (!todo) {
+//       return res.status(404).send();
+//     }
+//
+//     res.send({todo});
+//   }).catch((e) => {
+//     res.status(400).send();
+//   });
+// });
+//
+// app.post('/users', (req, res) => {
+//   var body = _.pick(req.body, ['email', 'password']);
+//   var user = new User(body);
+//
+//   user.save().then(() => {
+//     return user.generateAuthToken();
+//   }).then((token) => {
+//     res.header('x-auth', token).send(user);
+//   }).catch((e) => {
+//     res.status(400).send(e);
+//   });
+// });
+//
+// app.get('/users/me', authenticate, (req, res) => {
+//   res.send(req.user);
+// });
+//
+// app.post('/users/login', (req, res) => {
+//   var body = _.pick(req.body, ['email', 'password']);
+//
+//   User.findByCredentials(body.email, body.password).then((user) => {
+//     return user.generateAuthToken().then((token) => {
+//       res.header('x-auth', token).send(user);
+//     });
+//   }).catch((e) => {
+//     res.status(400).send();
+//   });
+// });
+//
+// app.delete('/users/me/token', authenticate, (req, res) => {
+//   req.user.removeToken(req.token).then(() => {
+//     res.status(200).send();
+//   }, (e) => {
+//     res.status(400).send();
+//   });
+// });
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
 
